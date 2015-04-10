8bf60b50c56ba826b12e78ceea1c65eb7188c5fc
Jorge Paiva <fskjorge@gmail.com>
2015-04-10 20:44:11 +0100

    GG WP

--- Worms 0.0.1/Worms 0.0.1/Weapons.cs
+++ Worms 0.0.1/Worms 0.0.1/Weapons.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,27 +10,51 @@ namespace Worms_0._0._1
 {
     class Weapons
     {
-        private enum WeaponType
+        protected enum WeaponType
         {
             Rocket,
             MachineGun,
             GrenadeLauncher,
             ShotGun
         }
-        private WeaponType WeaponTypes;
-        private int Magazine;
-        private float range;
-        private Vector2 PositionRelativeToCharacter;
+        private string Name;
+        protected WeaponType WeaponTypes;
+        protected int Magazine;
+        protected float range;
+        protected Vector2 PositionRelativeToCharacter;
+        protected MouseState Mouse = new MouseState();
+        protected float rotation;
+        private Texture2D textura;
         //MUDAR ISTO RELATIVAMENTE AS TEXTURAS!
+        /// <summary>
+        ///basicamente ver o tamanho das texturas dos characters!!!
+        /// </summary>
         private int TextureWidth = 20, TextureWheight = 40;
         //==================================================
 
-        private Weapons(Characters Char, WeaponType weaponType, int magazine, float range)
+        public Weapons(string name, Characters Char, WeaponType weaponType, int magazine, float range)
         {
+            this.Name = name; 
             this.PositionRelativeToCharacter = new Vector2(Char.CharacterPosition().X + (TextureWidth - 2), Char.CharacterPosition().Y - (this.TextureWheight/2));
             this.WeaponTypes = weaponType;
             this.Magazine = magazine;
             this.range = range;
+            rotation = 0;
+        }
+
+        public void Load()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
         }
 
 
--- Worms 0.0.1/Worms 0.0.1/WeaponsHandler.cs
+++ Worms 0.0.1/Worms 0.0.1/WeaponsHandler.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Worms_0._0._1
 {
-    class WeaponsHandler
+    class WeaponsHandler : Weapons
     {
+        static public List<Weapons> PoliceStationBasement = new List<Weapons>();
+        private static int Rndp;
+
+        static void InitList(Characters CharacterActive)
+        {
+            PoliceStationBasement.Add(new Weapons("Killerino", CharacterActive, WeaponType.MachineGun, 10, 25));
+            PoliceStationBasement.Add(new Weapons("Kiss My Ass", CharacterActive, WeaponType.ShotGun, 5, 5));
+            PoliceStationBasement.Add(new Weapons("LOL BOOM", CharacterActive, WeaponType.Rocket, 2, 30));
+            PoliceStationBasement.Add(new Weapons("KABOOM", CharacterActive, WeaponType.GrenadeLauncher, 4, 25));
+        }
+
+        static Weapons RandomDrop()
+        {
+            Random Rnd = new Random();
+            Rndp = Rnd.Next(0,3);
+            return PoliceStationBasement[Rndp];
+        }
+
+        static Weapons GetWeapon(int Weapon)
+        {
+            return PoliceStationBasement[Weapon];
+        }
     }
 }
