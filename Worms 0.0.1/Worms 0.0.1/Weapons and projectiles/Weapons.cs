d68bcd18b72dd391cb6a790937d1263acf463a00
Jorge Paiva <fskjorge@gmail.com>
2015-04-12 14:52:17 +0100

    So ando a trabalhar nas armas

--- Worms 0.0.1/Worms 0.0.1/Weapons and projectiles/Weapons.cs
+++ Worms 0.0.1/Worms 0.0.1/Weapons and projectiles/Weapons.cs
@@ -19,8 +19,10 @@ namespace Worms_0._0._1
         protected WeaponType WeaponTypes;
         protected int Magazine;
         protected float range;
+        protected static int previousWeapon = 0;
         protected Vector2 PositionRelativeToCharacter;
         protected float rotation;
+
 
         protected Texture2D textura;
         private SpriteFont font;
@@ -35,10 +37,7 @@ namespace Worms_0._0._1
         protected int TextureWidth = 10, TextureWheight = 15;
         //==================================================
 
-        public Weapons()
-        {
-
-        }
+        public Weapons(){ }
 
         public Weapons(string name, int serialNumber, Characters Char, WeaponType weaponType)
         {
@@ -58,13 +57,45 @@ namespace Worms_0._0._1
 
         public void Update(GameTime gameTime, Characters Char)
         {
-            PositionRelativeToCharacter = new Vector2(Char.CharacterPosition().X, Char.CharacterPosition().Y); ;
+            Input.Update();
+            PositionRelativeToCharacter = new Vector2(Char.CharacterPosition().X + 5, Char.CharacterPosition().Y);
 
-            if (Keyboard.GetState().IsKeyDown(Keys.D1)){
+
+            if (Input.IsPressed(Keys.D1)){
                 weaponCodeChoosen = 0;
+                WeaponsHandler.GetWeapon(weaponCodeChoosen);
+                previousWeapon = 0;
+
+                //''''''''''''
+                //test only!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                //Console.WriteLine("Weapon 1 Type: " + WeaponsHandler._GetWeapon(0).getWeaponState());
+                //Console.WriteLine("Weapon 2 Type: " + WeaponsHandler._GetWeapon(1).getWeaponState());
+                //Console.WriteLine("Weapon 3 Type: " + WeaponsHandler._GetWeapon(2).getWeaponState());
+                //Console.WriteLine("Weapon 4 Type: " + WeaponsHandler._GetWeapon(3).getWeaponState());
+                Console.WriteLine("\n");
+                Console.WriteLine("Active weapon Name " + WeaponsHandler.getActiveWeapon().getName());
+                Console.WriteLine("Active weapon angle: " + rotation);
+                Console.WriteLine("\n");
+                //test only!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                //''''''''''''
             }
-            else if (Keyboard.GetState().IsKeyDown(Keys.D2)){
+            else if (Input.IsPressed(Keys.D2)){
                 weaponCodeChoosen = 1;
+                WeaponsHandler.GetWeapon(weaponCodeChoosen);
+                previousWeapon = 1;
+
+                //''''''''''''
+                //test only!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                //Console.WriteLine("Weapon 1 Type: " + WeaponsHandler._GetWeapon(0).getWeaponState());
+                //Console.WriteLine("Weapon 2 Type: " + WeaponsHandler._GetWeapon(1).getWeaponState());
+                //Console.WriteLine("Weapon 3 Type: " + WeaponsHandler._GetWeapon(2).getWeaponState());
+                //Console.WriteLine("Weapon 4 Type: " + WeaponsHandler._GetWeapon(3).getWeaponState());
+                Console.WriteLine("\n");
+                Console.WriteLine("Active weapon Name: " + WeaponsHandler.getActiveWeapon().getName());
+                Console.WriteLine("Active weapon angle: " + rotation);
+                Console.WriteLine("\n");
+                //test only!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                //''''''''''''
             }
                
             //rever
@@ -76,6 +107,7 @@ namespace Worms_0._0._1
             float x = (float)PositionRelativeToCharacter.X - mousePos.X;
             float rot = (float)Math.Atan2(x,y);
             rot += (float)Math.PI/2f;
+            //Console.WriteLine("Angle: " + rot);
             rotation = rot;
             //rever
 
@@ -84,9 +116,9 @@ namespace Worms_0._0._1
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(this.textura, PositionRelativeToCharacter, null , Color.White, this.rotation, new Vector2(0, 0), 1f, SpriteEffects.None, 0f);
-            spriteBatch.DrawString(font, "Weapon Name: " + WeaponsHandler.GetWeapon(weaponCodeChoosen).getName(), new Vector2(500f, 500f), Color.White);
-            spriteBatch.DrawString(font, "Weapon Type: " + WeaponsHandler.GetWeapon(weaponCodeChoosen).getWeaponType().ToString(), new Vector2(500f, 525f), Color.White);
+            spriteBatch.Draw(this.textura, PositionRelativeToCharacter, null , Color.White, this.rotation, new Vector2(5, 0), 1f, SpriteEffects.None, 0f);          
+            spriteBatch.DrawString(font, "Weapon Name: " + WeaponsHandler._GetWeapon(weaponCodeChoosen).getName(), new Vector2(500f, 500f), Color.White);
+            spriteBatch.DrawString(font, "Weapon Type: " + WeaponsHandler._GetWeapon(weaponCodeChoosen).getWeaponType().ToString(), new Vector2(500f, 525f), Color.White);
 
             spriteBatch.DrawString(font, "test\nPress 1 - first weapon" , new Vector2(200f, 550f), Color.White);
             spriteBatch.DrawString(font, "Press 2 - second weapon", new Vector2(200f, 600f), Color.White);
@@ -101,9 +133,9 @@ namespace Worms_0._0._1
         }
 
         public void setWeaponState(){
-            if (this.activeState == false)
-                this.activeState = true;
-            else this.activeState = false;
+            if (this.activeState == true)
+                this.activeState = false;
+            else this.activeState = true;
         }
 
         public WeaponType getWeaponType(){
--- Worms 0.0.1/Worms 0.0.1/Weapons and projectiles/WeaponsHandler.cs
+++ Worms 0.0.1/Worms 0.0.1/Weapons and projectiles/WeaponsHandler.cs
@@ -12,37 +12,33 @@ namespace Worms_0._0._1
         static private List<string> NamesAndStuff = new List<string>();
         private static int Rndp;
 
-        static public void InitList(Characters CharacterActive, ContentManager content)
-        {
+        static public void InitList(Characters CharacterActive, ContentManager content){
             NamesAndStuff.Add("Weapon_name_not_Found");
             NamesAndStuff.Add("Killerino");
             NamesAndStuff.Add("Kiss My Ass");
             NamesAndStuff.Add("LOL BOOM");
+            NamesAndStuff.Add("KABOOM");
 
 
             PoliceStationBasement.Add(new Weapons(NamesAndStuff[1], 1, CharacterActive, WeaponType.MachineGun));
             PoliceStationBasement.Add(new Weapons(NamesAndStuff[2], 5, CharacterActive, WeaponType.ShotGun));
             PoliceStationBasement.Add(new Weapons(NamesAndStuff[3], 3, CharacterActive, WeaponType.Rocket));
-
-            //PoliceStationBasement.Add(new Weapons("KABOOM", CharacterActive, WeaponType.GrenadeLauncher, 4, 25));
+            PoliceStationBasement.Add(new Weapons(NamesAndStuff[4], 4,CharacterActive, WeaponType.GrenadeLauncher));
+            PoliceStationBasement[0].setWeaponState();
         }
 
-        static public Weapons RandomDrop()
-        {
+        static public Weapons RandomDrop(){
             Random Rnd = new Random();
             Rndp = Rnd.Next(0, 3);
             return PoliceStationBasement[Rndp];
         }
 
-        static public Weapons _GetWeapon(int weapon)
-        {
+        static public Weapons _GetWeapon(int weapon){
             return PoliceStationBasement[weapon];
         }
 
-        static public Weapons getActiveWeapon()
-        {
-            for (int i = 0; i < PoliceStationBasement.Count(); i++)
-            {
+        static public Weapons getActiveWeapon(){
+            for (int i = 0; i < PoliceStationBasement.Count(); i++){
                 if (PoliceStationBasement[i].getWeaponState() == true)
                     return PoliceStationBasement[i];
             }
@@ -51,10 +47,7 @@ namespace Worms_0._0._1
 
         static public Weapons GetWeapon(int Weapon)
         {
-            foreach (Weapons weapon in PoliceStationBasement){
-                if (weapon.getWeaponState() == true)
-                    weapon.setWeaponState();
-            }
+            PoliceStationBasement[previousWeapon].setWeaponState();
             PoliceStationBasement[Weapon].setWeaponState();
             return PoliceStationBasement[Weapon];
         }
